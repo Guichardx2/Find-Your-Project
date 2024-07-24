1f03179589afa49cb01ae823f885126b1ffe07f8
Guichard <jeancharles.g.guichard@gmail.com>
2024-07-24 20:25:13 -0300

    Alteração layout home

--- Main.py
+++ Main.py
@@ -1,32 +1,50 @@
 from tkinter import *
-from Cadastro import * 
+from cadastro import * 
 import customtkinter
+from PIL import Image, ImageTk
 
-class Home(Tk):
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("dark-blue")
-    inicio= customtkinter.CTk()
-    inicio.title('Find Your Process')
-    inicio.resizable(False, False)
-    inicio.geometry("700x400")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+home= customtkinter.CTk()
+home.title('Login')
+home.resizable(False, False)
+home.geometry("700x400")
     
-    #img da tela
-    img= PhotoImage(file= "bemvindo-transformed.png")
-    img= img.subsample(2,2)
-    label_img= customtkinter.CTkLabel(master= inicio, image= img)
-    label_img.grid(row= 0, column= 1)
-    label_img.place(x=-115, y=65)
+#img da tela
+img_path= "images/pasta.png"
+img= Image.open(img_path)
+img= img.resize((400,400))
+img= ImageTk.PhotoImage(img)
 
-    #frame
-    frame= customtkinter.CTkFrame(master= inicio, width= 350, height=396)
-    frame.pack(side= RIGHT)
+#frame imagem
+frame_img= customtkinter.CTkFrame(master=home, width=350, height=400)
+frame_img.pack(side=LEFT)
 
-    label_frame= customtkinter.CTkLabel(master=frame, text="Bem-vindo a organização do seu trabalho!", font= ('Sans' , 16))
-    label_frame.place(x=25, y=10)
+#label imagem
+label_img= customtkinter.CTkLabel(master=frame_img, image=img, bg_color="lightblue", text=None)
+label_img.place(x=-10,y=5)
 
-    # welcome= Label(inicio, text= "Bem-vindo a um pequeno programa de ajuda na organização!").grid(column=0, row= 0, padx=10, pady=10 )
-    # txt= Label(inicio, text= "Escolha uma das opções abaixo, o que deseja fazer? Vai no teu tempo!").grid(column=0, row=1, pady=10)
-    # btn_Cadastro= Button(inicio, text= "Cadastrar processo", command= pg_cadastro).grid(column=0, row=2, pady=5)
-    # btn_Consulta= Button(inicio, text= "Consultar um processo", command= inicio.destroy).grid(column=0, row=3)
-    
-    inicio.mainloop()    
+#frame
+frame_login= customtkinter.CTkFrame(master=home, width=350, height=400)
+frame_login.pack(side=RIGHT)
+
+#frame de login
+label_login= customtkinter.CTkLabel(master=frame_login, text="Bem-vindo ao sistema!", font=("Roboto", 25))
+label_login.place(x=50, y=10)
+
+#widgtes
+user_name_label= customtkinter.CTkLabel(master=frame_login, text="Usuário", font=("Roboto", 14)).place(x=30, y=70)
+user_name = customtkinter.CTkEntry(master=frame_login, placeholder_text="Digite seu usuário", width=300, height=35, font=("Roboto", 14), corner_radius=15).place(x=30, y=100)
+user_name_warning= customtkinter.CTkLabel(master=frame_login, text="*Este campo é obrigatório*", text_color="green", font=("Roboto", 12), fg_color="transparent").place(x=30, y=140)
+
+password_label= customtkinter.CTkLabel(master=frame_login, text="Senha", font=("Roboto", 14)).place(x=30, y=180)
+password = customtkinter.CTkEntry(master=frame_login, placeholder_text="Digite sua seha", width=300, height=35, font=("Roboto", 14), show="*", corner_radius=15).place(x=30, y=210)
+password_warning= customtkinter.CTkLabel(master=frame_login, text="*Este campo é obrigatório*", text_color="green", font=("Roboto", 12), fg_color="transparent").place(x=30, y=250)
+
+#botão
+login_button= customtkinter.CTkButton(master=frame_login, text="Entrar", width=300, height=35, font=("Roboto", 14), corner_radius= 20).place(x=30, y=300)
+
+cadastro_label= customtkinter.CTkLabel(master=frame_login, text="Ainda não tem uma conta?", font=("Roboto", 12)).place(x=30, y=370)
+cadastro_button= customtkinter.CTkButton(master=frame_login, text="Cadastre-se", width=100, height=25, font=("Roboto", 12)).place(x=180, y=370)
+home.mainloop()    
